2c1ccf8868004d1b5d4c4a87493b999573977d0a
Vladislav <kelsenelenelvial@mail.ru>
2021-03-20 21:56:36 +0300

    scrolling page to top

--- TOP7-march2021/js/main.js
+++ TOP7-march2021/js/main.js
@@ -106,3 +106,16 @@ callbackModalOverlay.addEventListener("click", event => {
 });
 
 
+/*===== Scroll UP =====*/
+const scrollUpBtn = document.querySelector(".scrollup-btn");
+
+const scrollUp = () => {
+	let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+	if (currentScroll > 0) {
+		window.requestAnimationFrame(scrollUp);
+		window.scrollTo(0, currentScroll - (currentScroll / 5));
+	}
+};
+scrollUpBtn.addEventListener("click", () => {
+	scrollUp();
+});
